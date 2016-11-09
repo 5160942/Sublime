4879273ec4a52c7feffa53b956387aed4c053bea
5160942 <5160942@gmail.com>
2016-11-09 18:59:16 -0500

    Nov 9 2016 19:00

---
+++ jQuery/ch2/tableSelector.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+	console.log($('#celebs tbody tr:even').length + ' elements');
+});
+
+$(document).ready(function() {
+	/*var backgroundColor = [];
+	backgroundColor = $('#celebs tbody tr:first').css('background-color');
+	console.log(backgroundColor);
+	$('#celebs tbody tr:even').css('background-color','#D3D3D3');
+	backgroundColor = $('#celebs tbody tr:even').css('background-color');
+	console.log(backgroundColor);
+	$('#celebs tbody tr:even').css('color', 'white');
+	$('#celebs tbody tr:even').css({
+		'background-color': '#dddddd', 
+		'color': '#666666',
+		'font-size' : '14px',
+		'line-height' : '2.5em'
+	});*/
+	$('#celebs tbody tr:even').addClass('zebra');
+});
+
+$(document).ready(function() {
+	$('#news').addClass('zebra');
+	$('#news .spoiler').css('font-size','20px');
+});
+
+$(document).ready(function() {
+		$('#disclaimer').hide();
+		$('#hideButton').click(function() {
+			$('#disclaimer').hide();
+		});
+});
+
+$(document).ready(function() {
+	$('#showButton').click(function() {
+		$('#disclaimer').show();
+	})
+});
